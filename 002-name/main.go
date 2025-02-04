c4419b0787cd32fca04e47ad8b37dcc24893eb97
Hanson Ramkhalawon <hansonmru@gmail.com>
2025-02-04 14:30:07 +0100

    [100-exo] ex-2

---
+++ 002-name/main.go
@@ -0,0 +1,10 @@
+// Exercice : Créez un programme qui affiche votre nom, votre adresse et contient également des commentaires
+// - Utiliser le package fmt
+
+package main
+
+// import "fmt"
+
+func main() {
+	// Here goes your code
+}
---
+++ 002-name/solution/solution.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Printing my name
+	fmt.Println("Mon nom est Hanson")
+	// Printing my address!
+	fmt.Println("Mon adresse est : Avenue Charles de Gaulles")
+}
