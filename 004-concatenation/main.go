3b630038e3c8666ac8bba30dcabbc54b82fb5deb
Hanson Ramkhalawon <hansonmru@gmail.com>
2025-02-20 14:57:06 +0100

    [100-exo] ex-4

---
+++ 004-concatenation/main.go
@@ -0,0 +1,11 @@
+// Exercice : concaténer deux variables de chaîne et afficher son résultat
+// Une variable doit s'appeler « helloWorld » et l'autre « itsmemario »
+// Il doit y avoir un espace entre les deux
+
+package main
+
+func main() {
+	// Here goes your code
+	// var string1, string2 string
+	// ...
+}
---
+++ 004-concatenation/solution/solution.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Creating new variable called helloWorld
+	var helloWorld, itsMeMario string
+	helloWorld = "Hello World!"
+	itsMeMario = "It's a me, Mario"
+	// Print the variable
+	fmt.Println(helloWorld + " " + itsMeMario)
+}
+
+// To run the program:
+// - go run solution.go
