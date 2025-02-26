ce224cfbef25dd25e3da40cbc12c23e394098d3b
Hanson Ramkhalawon <hansonmru@gmail.com>
2025-02-26 23:19:54 +0100

    [100-exo] ex-5

---
+++ 005-ascii/main.go
@@ -0,0 +1,11 @@
+// Exercice : avec une seule variable string, utilisez l'accès au premier caractère avec l'index de chaîne
+// Ceci n'est valable qu'avec les caractères ASCII, et la valeur d'impression sera le numéro ASCII
+// string[n] est la façon dont vous devez accéder à la valeur
+
+package main
+
+func main() {
+	// Here goes your code
+	// var string1 string
+	// ...
+}
---
+++ 005-ascii/solution/solution.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Creation de la variale helloWorld
+	var helloWorld string
+	helloWorld = "Hello World!"
+	// Affichage de la valeur de la première lettre de la variable helloWorld
+	fmt.Println(helloWorld[0])
+}
